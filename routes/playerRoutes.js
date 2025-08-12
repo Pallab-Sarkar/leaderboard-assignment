08d1662236c1d21a2de38382790dae0092d3ffb8
Pallab Sarkar <pallabsarkar31@gmail.com>
2025-08-12 18:33:24 +0530

    add message

--- routes/playerRoutes.js
+++ routes/playerRoutes.js
@@ -3,13 +3,25 @@ import PlayerModel from "../model/playerModel.js";
 
 const router = express.Router();
 
+router.post("/create", async (req, res) => {
+  try {
+    const player = await PlayerModel.create(req.body);
+    res.json({ message: "Player created successfully", data: player });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 router.get("/top/:count", async (req, res) => {
   const count = parseInt(req.params.count) || 10;
   try {
     const topPlayers = await PlayerModel.find()
       .sort({ score: -1 })
       .limit(count);
-    res.json(topPlayers);
+    res.json({
+      message: `Top ${count} players details fetched successfully.`,
+      data: topPlayers,
+    });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -25,7 +37,10 @@ router.get("/filter", async (req, res) => {
     const filteredPlayers = await PlayerModel.find(query)
       .sort({ score: -1 })
       .limit(parseInt(count) || 10);
-    res.json(filteredPlayers);
+    res.json({
+      message: "Specific players list fetched successfully.",
+      data: filteredPlayers,
+    });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
